15cd16d86288561f7144abb2dc22d40978ec89a2
Mk1806 <makandamilka@gmail.com>
2022-09-18 16:30:21 +0300

    four

--- azukrea.py
+++ azukrea.py
@@ -6,3 +6,14 @@ t = [Turtle(), Turtle()]
 x = 6
 colors=["red","yellow","blue","lime",]
 for index, i in enumerate(t):
+    i.speed(0)
+    i.color("white")
+    i.shape("circe")
+    i.shapesize(0.3)
+    i.width(3)
+    i.pu()
+    i.seth(90)
+    i.fd(350)
+    i.seth(-180)
+    i.pd()
+    
